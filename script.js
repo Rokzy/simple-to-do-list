a4441bee73a8d15a74b2ff4d3e78a1964bce2fa7
Rokzy <69007741+Rokzy@users.noreply.github.com>
2022-11-22 19:12:39 +0100

    updated to do list

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
-function newTask() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("userInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert ("Write down your task!");
-    } else {
-      document.getElementById("taskList").appendChild(li);
-    }
-    document.getElementById("userInput").value = "";
-  
+function newTask(inputValue) {
+  var li = document.createElement("li");
+  li.textContent = inputValue;
+  var span = document.createElement("span");
+  span.textContent = "☑️"; // ✅
+  li.appendChild(span);
+  span.addEventListener("click", function (event) {
+    newStyle(event);
+  });
+  var span2 = document.createElement("span");
+  span2.textContent = "❌";
+  li.appendChild(span2);
+  span2.addEventListener("click", function (event) {
+    removeItem(event);
+  });
+  if (inputValue === "") {
+    alert("Write down your task!");
+  } else {
+    document.getElementById("taskList").appendChild(li);
+  }
+  document.getElementById("userInput").value = "";
 }
 
+function handleClick() {
+  var inputValue = document.getElementById("userInput").value;
+  newTask(inputValue);
+}
+
+let tasks = [
+  "dog walk",
+  "car wash",
+  "cleaning",
+  "grocery shopping",
+  "laundry",
+  "dinner",
+  "workout",
+  "read a book",
+  "watch a movie",
+  "go to the gym",
+  "go to the beach",
+  "go to the park",
+  "go to the mall",
+  "go to the movies",
+];
+
+tasks.forEach(newTask);
+
+function removeItem(event) {
+  debugger;
+  event.target.parentElement.remove();
+}
+
+function newStyle(event) {
+  debugger;
+  let newColor = "";
+
+  if (event.target.parentElement.style.color === "blue") {
+    newColor = "black";
+  } else {
+    newColor = "blue";
+  }
+
+  event.target.parentElement.style.color = newColor;
+}
